3a3c8f91d29d0d0f082d6d723d3dfe6e39791d70
rishic1207 <emailrishic@gmail.com>
2025-06-14 16:15:19 -0400

    got server running and model working (kinda)

--- app/model/model.py
+++ app/model/model.py
@@ -1,1 +1,9 @@
-#File for the model
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+def compute_score(resume_text, job_text):
+    documents = [resume_text, job_text]
+    vectorizer = TfidfVectorizer(stop_words="english")
+    tfidf_matrix = vectorizer.fit_transform(documents)
+    score = cosine_similarity(tfidf_matrix[0:1], tfidf_matrix[1:2])[0][0]
+    return round(score * 100, 2)
--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for, session
 import os
 from app.utils.extract_resume import extract_text
 from app.utils.job_web_scraper import scrape_job
+from app.model.model import compute_score
 
 app = Flask(__name__)
 app.secret_key = "key"
@@ -26,11 +27,14 @@ def index():
         if not job_text and job_text_input:
             job_text = job_text_input
         if resume_text and job_text:
-            session["result"] = job_text #Just a placeholder until we get the model to work
+            score = compute_score(resume_text, job_text)
+            session["result"] = f"{score}"
         else:
             session["result"] = "Error: Either missing resume/job description or the job description website can not be extracted and the description needs to be extracted manually."
         return redirect(url_for("result"))
     
+    return render_template("index.html")
+
 @app.route("/result")
 def result():
     result = session.get("result", "No result to show")
