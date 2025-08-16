21ede43a1ff97270a059de0d5b5bf64de07cc60e
Arav Singh <aravsingh@live.com>
2025-08-16 16:11:34 -0400

    fixed errors

--- app/utils/extract_resume.py
+++ app/utils/extract_resume.py
@@ -1,3 +1,4 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:02c279d69024e2db73086d546309fd02b82c4111e8b3467556c524e21b9aad4f
-size 134
+from pdfminer.high_level import extract_text as pdfminer_extract
+
+def extract_text(pdf_path):
+    return pdfminer_extract(pdf_path)
